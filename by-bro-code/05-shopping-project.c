b6cfde171020affc5cd2bb9367a842e0afbdd432
Jasmeet Singh <jasmeetsonu990@gmail.com>
2025-06-06 15:08:18 +0530

    learning c

---
+++ by-bro-code/05-shopping-project.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+    //  SHOPPING CART PROGRAM
+
+    char item[50] = "";
+    float price = 0.0f;
+    int quantity = 0;
+    char currency = '$';
+    float total = 0.0f;
+
+    printf("What item would you like to buy?: ");
+    fgets(item, sizeof(item), stdin);
+
+    item[strlen(item) - 1] = "\0";
+
+    printf("What is the price for each?: ");
+    scanf("%f", &price);
+
+    printf("how many would you like?: ");
+    scanf("%d", &quantity);
+
+    total = price * quantity;
+    printf("Your total is %.2f", total);
+
+    return 0;
+}
