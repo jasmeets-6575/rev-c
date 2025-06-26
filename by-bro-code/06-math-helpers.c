ad529dbfbd690f3307884f3f701ca16b4c64b48d
Jasmeet Singh <jasmeetsonu990@gmail.com>
2025-06-26 15:09:23 +0530

    learning c

---
+++ by-bro-code/06-math-helpers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    int x = 9;
+
+    x = sqrt(x);
+
+    // roundoff 
+    // x = round(x);
+
+    // x = ceil(x);
+    // x = floor(x);
+
+    // absolute ( +ve)
+    // x = abs(x);
+
+    // x = log(x); 
+    // x = sin(x);
+    // x = cos(x);
+    // x = tan(x);
+
+    printf("%d", x);
+
+    return 0;
+}
