31362656789800550c0d8db48b3a0a8cec9fbb68
Jasmeet Singh <jasmeetsonu990@gmail.com>
2025-07-07 16:00:56 +0530

    learning c

---
+++ by-bro-code/20-pointers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void birthday(int* age);
+
+int main() {
+
+    // pointer = A variable that stores the mempry address of another variable .
+    //         benefit : They help avoid wasting memory by allowing you to pass the address of
+    //         the large data strucutre instead of copying the entire data
+
+    int age = 25;
+    // printf("%p", age);
+    
+    int *pAge = &age;
+    printf("%p", pAge);
+
+    birthday(pAge);
+
+    return 0;
+}
+
+void birthday(int* age){
+    (*age)++;
+}
